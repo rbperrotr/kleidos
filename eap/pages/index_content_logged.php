d07c655d330213cba98acecbaaf0fa56b491760a
Raphaël Bernard-Perrottey <raphael.bernardperrottey@thomsonreuters.com>
2014-04-23 08:49:54 +0200

    Correct intro

--- eap/pages/index_content_logged.php
+++ eap/pages/index_content_logged.php
@@ -9,23 +9,23 @@
 	<p>This all started with your founder, Julius Reuters. Like one of my fathers, he was a member of a small community, holding a secret, or rather part of a big secret, which would play a critical role for the humankind in the future.</p>
 	<p>This was a piece of knowledge, a possibility; pretty advanced, but incomplete.</P>
 	<p>It has been transmitted across centuries to small groups of people, whose role was to fine-tune the knowledge, according to the progress of science. But as well, to keep it away from lust, avoid it being used with bad intentions, and then transmit it to the next group of proper researchers. They had to pass it on to a few people who would deserve holding such a big responsibility.</p>
-	<p>I am the last holder of that secret for a short while, and it is now time for me to pass it on. Times have changed, and there is no point in looking for isolated researchers anymore. Time has come to accelerate the progress, and I want to hand this knowledge over to a bigger group of people, across the globe; I want it shared.</p>
+	<p>I am the only remaining holder of that secret for a short while, and it is now time for me to pass it on. Times have changed, and there is no point in looking for isolated researchers anymore. Time has come to accelerate the progress, and I want to hand this knowledge over to a bigger group of people, across the globe; I want it shared.</p>
 	<p>You have all been selected, because I believe you match with the criteria, you deserve being made aware.</p>
-	<p>Since I have always been a player by nature, despite the big duty of holding such a secret, I will give you a challenge, a kind of treasure hunt, which will be linked to your coming Innovathon. You will have not only to progress the research, but to take on the mystery together.</p>
+	<p>Since I have always been a player by nature, despite the big duty of holding such a secret, I will give you a challenge, a kind of scavenger hunt, which will be linked to your coming Innovathon. You will have not only to progress the research, but to take on the mystery together.</p>
 	<p>At the end of the hunt, you will see it is worth it. You will have to solve a number of enigmas, understand the logic, and use all the elements you will have collected to go to the end. I will rely on your morality to make a good usage of what you will discover.</p>
 	<br>
 	<h2>The challenge</h2>
 	<p>The challenge will take shape in a number of enigmas that will be revealed progressively to a few people I have selected, the guardians. They will publish enigmas to you all.</p>
 	<p>Don’t even think of trying to corrupt them, they only have a very partial view and wouldn’t be of much help…</p>
 	<p>The guardians will broadcast the enigmas, validate your answers, as well as grant some hint codes to you, according to your efficiency (more details in the help section).</p>
-	<p>The theme will be the history of the humankind. You will have to be watchful, patient, and use your brain to find the answers. Some tracks will be provided to you, but often in a hidden way… You will not need any deep knowledge of any specific topic. But your wisdom and cleverness will be most needed for finding your way.</p>
-	<p>I have designed these enigmas for different type of mind-sets, so that you have to collaborate not only locally, but as well across locations. That is diversity which will permit to solve the whole hunt and reveal the secret. Together you will succeed…</p>
+	<p>The theme will be the history of the humankind. You will have to be watchful, patient, and use your brain to find the answers. Some tracks will be provided to you, but often in a hidden way… You will not need any deep knowledge of any specific topic. But your wisdom and cleverness will be most needed for finding the right way.</p>
+	<p>I have designed these enigmas for different type of mind-sets, so that you have to collaborate not only locally, but as well across locations. That is diversity which will permit to solve the whole challenge and reveal the secret. Together you will succeed…</p>
 	<p>The first ones to reveal the secret will be awarded. Not only will they take on the responsibility to own this big secret, but they will have to control its broadcasting. They will have to be trustful and have a strong mind to resist temptations.</p>
 	<p>Some advices to help you in the hunt are given below.</p>
 	<p>Good luck to all!</p>
 	<h2>Help</h2>
 	<p>There will be two phases. The first phase stops when the Innovathon starts. This is when the second phase starts.</p>
-	<p>Answers will always have to be provided in English, or as spelled in English documents and websites. If the answer doesn’t translate in English, please use the original spelling.</p>
+	<p>Answers will always have to be provided in English, or as spelled in English documents and websites. If the answer doesn’t translate in English, please use the spelling in the original language.</p>
 	<p>There are different types of enigmas. But you will have to find out more...</p>
 	<p>Most or all of the answers to enigmas will be reused directly or indirectly at some point, this is linked as well to the different types of enigmas.</p>
 	<p>In the end, you will have to find how to connect the various pieces of information together.</p>
