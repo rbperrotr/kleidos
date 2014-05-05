cd4579dcb923c3dc4d5a734f21a25ab8964def40
Raphaël Bernard-Perrottey <raphael.bernardperrottey@thomsonreuters.com>
2014-05-06 00:50:10 +0200

    initialize ANSWER_NOTIF

--- index.php
+++ index.php
@@ -20,6 +20,7 @@
 					require('controler/truser.php');
 					
 					$_SESSION['EMAILNOTIF']=1;
+					$_SESSION['ANSWERS_NOTIF']=0;
 					//$_SESSION['DEBUG']=1; // to comment in normal use
 					$login = false;
 					echo_debug("INDEX | Starting<br>");
